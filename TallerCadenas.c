9b78d5d5d398e345a0346987145b2fbbd9c0126d
Cristian Santiago <cristian.porras02@uptc.edu.co>
2025-09-25 14:55:33 -0500

    Declaracion funcion contar palabras

--- TallerCadenas.c
+++ TallerCadenas.c
@@ -25,14 +25,50 @@ void contarCaracteres(char *nombrePropio, char caracter) {
     printf("El caracter %c se encuentra %i veces en la cadena de texto", caracter, contador);
 }
 
+int contarPalabras(char *texto, char *palabra) {
+    int contador = 0;
+    char textoAlmacenado[100];
+    int j = 0;
+
+    for (int i = 0; i <= strlen(texto); i++) {
+        if (texto[i] == ' ' || texto[i] == '\0') {
+            textoAlmacenado[j] = '\0';
+            if (strcmp(textoAlmacenado, palabra) == 0) {
+                contador++;
+            }
+            j = 0;
+        } else {
+            textoAlmacenado[j] = texto[i];
+            j++;
+        }
+    }
+    return contador;
+}
+
 int main(){
     char nombrePropio[100];
     printf("Ingrese su nombre: ");
     fgets(nombrePropio, sizeof(nombrePropio), stdin);
     NombrePropio(nombrePropio);
+
     char caracter;
     printf("\nIngresa el caracter que deseas encontrar: ");
     scanf("%c", &caracter);
     contarCaracteres(nombrePropio, caracter);
-    
+
+    getchar();
+    char texto[200];
+    char palabra[50];
+    printf("\nIngrese un texto: ");
+    fgets(texto, sizeof(texto), stdin);
+    texto[strcspn(texto, "\n")] = 0;
+
+    getchar();
+    printf("\nIngrese la palabra que desea buscar: ");
+    fgets(palabra, sizeof(palabra), stdin);
+    palabra[strcspn(palabra, "\n")] = 0;
+
+    int nVeces = contarPalabras(texto, palabra);
+    printf("La palabra '%s' aparece %d veces en el texto.\n", palabra, nVeces );
+
 }
